b99b47c8dd98c53ccdb9d5524e31d61b34be2bec
Yi-En Su <fonyou1337@gmail.com>
2016-07-06 23:10:19 +0800

    Make it available to upload to pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ml-toolkit',    # This is the name of your PyPI-package.
+    packages=['ml_toolkit'],
+    version='0.04',                          # Update the version number for new releases
+	description = 'Tools for doing machine learning',
+	author='ENsu'
+)
