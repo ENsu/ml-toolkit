255f4d7726bd34fc4099c9051bd0a921bfd206b5
Yi-En Su <fonyou1337@gmail.com>
2016-08-25 11:23:44 +0800

    Revice geo to add function

--- ml_toolkit/geo.py
+++ ml_toolkit/geo.py
@@ -1,16 +1,21 @@
 import requests
 
 
-def lonlat2add(long, lat):
-    parameter = {'latlng': "%s,%s" % (lat, long)}
+def lnglat2add(lng, lat):
+    parameter = {'latlng': "%s,%s" % (lat, lng)}
     r = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=parameter)
     r = r.json()
+    if len(r['results']) == 0:
+        return None
     return r['results'][0]['formatted_address']
 
 
-def lonlat2city(long, lat):
-    parameter = {'latlng': "%s,%s" % (lat, long)}
+def lnglat2city(lng, lat):
+    parameter = {'latlng': "%s,%s" % (lat, lng)}
     r = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=parameter)
     r = r.json()
+    if len(r['results']) == 0:
+        return None
     city_info = r['results'][0]['address_components']
-    return (city_info[-1]['long_name'], city_info[-2]['long_name'], city_info[-3]['long_name'], city_info[-4]['long_name'])
+    return ','.join([i['long_name'] for i in city_info])
+    # return (city_info[-1]['long_name'], city_info[-2]['long_name'], city_info[-3]['long_name'], city_info[-4]['long_name'])
